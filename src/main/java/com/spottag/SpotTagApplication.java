e6f51c533f48441f4c091f05dacc53a62d74f7aa
Seonggil-Jeong <sgjeong1108@gmail.com>
2023-08-27 03:04:18 +0900

    [#2] feat : add application-private.yml

--- src/main/java/com/spottag/SpotTagApplication.java
+++ src/main/java/com/spottag/SpotTagApplication.java
@@ -1,17 +1,25 @@
 package com.spottag;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@PropertySource("classpath:application-private.yml")
+@RequiredArgsConstructor
 public class SpotTagApplication {
+    private final Environment env;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpotTagApplication.class, args);
     }
+
     @PostConstruct
     public void started() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
